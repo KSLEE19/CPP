e1a897f90f0326ff87f152591174fa5137c1e71f
KSLEE19 <kingslee0619@gmail.com>
2023-05-13 23:27:50 +0900

    유클리드 호제법

---
+++ BOJ_2609.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int mod(int,int);
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int mod_num;
+    if(a>b) mod_num=mod(a,b);
+    else mod_num=mod(b,a);
+    a/=mod_num;
+    b/=mod_num;
+    cout<<mod_num<<"\n";
+    cout<<mod_num*a*b;
+}
+
+int mod(int a,int b){
+    int ret;
+    if(a%b==0) return b;
+    else ret=mod(b,a%b);
+    return ret;
+}
